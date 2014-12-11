945490c099890c11e38d405743a52f0b03f389ce
Gmadges <i7239468@bournemouth.ac.uk>
2014-12-11 11:05:52 +0000

    updated uml, and added skeleton of input header

---
+++ input/input.h
@@ -0,0 +1,26 @@
+#ifndef CAR_H__
+#define CAR_H__
+
+class input
+{
+
+public:
+
+    input();
+    ~input();
+
+    bool buttonUP();
+    bool buttonDOWN();
+    bool buttonLEFT();
+    bool buttonRIGHT();
+    bool buttonSELECT();
+
+
+private:
+
+
+
+};
+
+
+#endif
