78456e9c30c7dcae37b309e7b6a3338c31876d83
Tom Minor <i7245143@bournemouth.ac.uk>
2014-12-19 04:32:31 +0000

    Added more example code

--- src/examples/cardUsage.cpp
+++ src/examples/cardUsage.cpp
@@ -6,11 +6,17 @@ int main()
   // Construct the card
   PlayingCard card1(Rank::ACE, Suit::SPADE );
   PlayingCard card2(Rank::ACE, Suit::SPADE );
+  PlayingCard card3(Rank::JACK, Suit::DIAMOND );
+
+  std::cout << "Rank of card1 is " << card1.RankString() << "\n";
+  std::cout << "Suit of card1 is " << card1.SuitString() << "\n";
 
   std::cout << card1 << std::endl;
   std::cout << card2 << std::endl;
+  std::cout << card3 << std::endl;
 
-  std::cout << bool(card1==card2) << "\n";
+  std::cout << "Cards " << (card1==card2 ? "are not" : "are") << " equal\n";
+  std::cout << "Cards " << (card1!=card2 ? "are" : "are not") << " equal\n";
 
   return 0;
 }
