8f854943880dc2c6ad8c7dd7160cd70d06506bfe
Gmadges <i7239468@bournemouth.ac.uk>
2015-01-20 17:31:30 +0000

    added fold ability to placebet

--- src/player/MainSketch/MainSketch.ino
+++ src/player/MainSketch/MainSketch.ino
@@ -7,6 +7,8 @@
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 
+player test(80, 2);
+
 void setup() 
 {
   // set up the LCD's number of columns and rows:
@@ -15,6 +17,13 @@ void setup()
 
 void loop() 
 {
-   
-  lcd.print("butt");
+  unsigned int bet; 
+  bet = test.placeBet(100, 50);
+  
+  lcd.clear();
+  
+  lcd.print(String(bet));
+  
+  delay(2000);
+  
 }
--- src/player/MainSketch/player.cpp
+++ src/player/MainSketch/player.cpp
@@ -21,46 +21,52 @@ uint16_t player::placeBet(uint16_t _max, uint16_t _min)
    while(!quit)
    {
      bool exit = false;
+     bool fold = false;
      
      uint16_t bet = _min;
-     uint8_t x;
+     //uint8_t x;
 
      lcd.clear();
 
      lcd.print("Place bet:");
 
-     //fixes it from selecting autimatically needs better fix.
+     //fixes it from selecting automatically needs better fix.
      delay(1000);
 
      while(!exit)
      {
       button.updateValue();
+      //clear the bottom line
       lcd.setCursor(0,1);
+   
+      if      (button.right() && bet <= (_max-10) && bet <= (m_money-10)) { bet+=10; fold = false;    }
+      else if (button.up() && bet < _max && bet < m_money)                { bet++; fold = false;      }
+      else if (button.down() && bet > _min && bet <= m_money)             { bet--; fold = false;      }
+      else if (button.left() && bet >= (_min+10) && bet < (m_money+10))   { bet-=10; fold = false;    }
+      else if (button.left() && bet == _min)                              { fold = true;              }
+      else if (button.down() && bet == _min)                              { fold = true;              }
+      else if (button.select())                                           { exit = true;              }
 
-      if      (button.right() && bet <= (_max-10) && bet < (m_money-10))  { bet+=10;    }
-      else if (button.up() && bet < _max && bet < m_money)                { bet++;      }
-      else if (button.down() && bet > _min && bet < m_money)              { bet--;      }
-      else if (button.left() && bet >= (_max+10) && bet < (m_money+10))   { bet-=10;    }
-      else if (button.select())                                           { exit = true;}
-
-      // have to use delay, no ideal
+      // have to use delay, not ideal
       delay(125);
 
       // print the bet
-      lcd.print(String(bet));
+      if(fold == true)       { lcd.print("FOLD");      }
+      else if(fold == false) { lcd.print(String(bet)+"       "); } 
      }
 
      lcd.clear();
-     lcd.print("Confirm bet: "+String(bet));
-      
      
+     if(fold == true)      { lcd.print("FOLD?:");                         }  
+     else if(fold == false){ lcd.print("Place bet of "+String(bet))+"?";  }
+      
      bool confirm = button.menuYesNo(1);
 
      if(confirm == true)
      {
        quit = true;
-       m_money = m_money - bet;
-       return bet;
+       if(fold == true)  { /*return fold*/   }
+       else              { m_money = m_money - bet; return bet; }
      }
   }
 }
@@ -77,6 +83,7 @@ void player::receiveCard()
 
 void player::setName()
 {
+  
 
 }
 
