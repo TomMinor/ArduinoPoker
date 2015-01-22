af78ed00c421293c56e693b11c7fe69f59d604e8
michellewu1098 <MichelleWu@MacBook-Pro-di-Michelle-Wu.local>
2015-01-22 01:42:15 +0000

    updated display function

--- src/player/MainSketch/pokerDisplay.h
+++ src/player/MainSketch/pokerDisplay.h
@@ -3,29 +3,26 @@
 
 #include "Arduino.h"
 #include <LiquidCrystal.h>
+#include "card.h"
 
 extern LiquidCrystal lcd;
 
 class display
 {
-  public:
-  
+public:
+    
     display(int _totCards);
     ~display();
     void displayCard( uint8_t _rank, uint8_t _suit, int _x, int _y, int _nCard );
     void createCustomChar();
-  
-  private:
-  
+    
+private:
+    
     int m_x;
     int m_y;
-    char m_rank[6];
-    char m_suit[8];
     int m_totCards;
     int m_nCard;
-
+    
 };
 
 #endif
-
-
