68604d9bd6d82b450ebc720c6dfe33ae5a7e0897
IdrisMiles <idris.miles@gmail.com>
2015-01-25 15:17:48 +0000

    merged origin/master containing robs new stuff

--- src/dealer/dealerLibMain.cpp
+++ src/dealer/dealerLibMain.cpp
@@ -164,12 +164,12 @@ int main()
             std::cout<<"num liveplayers: "<<table.getNumPlayers()<<"\n";
           }
 
-        table.dealHands();//call gui.dealCardTo(playerID,PlayingCard)
+        //table.dealHands();//call gui.dealCardTo(playerID,PlayingCard)
 
         //table.dealHands(pack);//call gui.dealCardTo(playerID,PlayingCard)
 
 
-        table.bet();//internally call gui.receiveBetFrom(playerID,amount)
+        //table.bet();//internally call gui.receiveBetFrom(playerID,amount)
 
 
         //Burning deck card method:
@@ -179,13 +179,13 @@ int main()
 //        burned->updateRect();
 //        burned->burn();
 
-        table.dealFlop();//internally call addPublicCard(PlayingCard) x3
+        //table.dealFlop();//internally call addPublicCard(PlayingCard) x3
         //table.dealFlop();//internally call addPublicCard(PlayingCard) x3
 
 
         //table.bet();//internally call gui.receiveBetFrom(playerID,amount)
                         //call gui.receiveBetFrom(playerID,Unit16 amount,bool isFirstBet)
-        table.dealRiverTurn();//internally calladdPublicCard(PlayingCard)
+        //table.dealRiverTurn();//internally calladdPublicCard(PlayingCard)
         //table.dealRiverTurn();//internally calladdPublicCard(PlayingCard)
 
         //table.bet();//internally call gui.receiveBetFrom(playerID,amount)
@@ -193,7 +193,7 @@ int main()
                         //call gui.receiveBetFrom(playerID,Unit16 amount,bool isFirstBet)
 
 
-        table.dealRiverTurn();//internally calladdPublicCard(PlayingCard)
+        //table.dealRiverTurn();//internally calladdPublicCard(PlayingCard)
         //table.dealRiverTurn();//internally calladdPublicCard(PlayingCard)
 
         //table.dealRiverTurn();//internally calladdPublicCard(PlayingCard)
